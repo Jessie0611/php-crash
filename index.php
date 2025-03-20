775f8412034340dc90ca3243dff92368c3cc969e
Jessie0611 <imjustme0611@hotmail.com>
2025-03-20 13:00:54 -0400

    Quick crash course refresher on php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*OUTPUT
+echo - output strings, numbers, html, etc.
+print - works like echo but can only take in a single argument
+print_r() - function, single calues and arrays
+print_r([1,2,3]);
+var_dump() - returns more info like data type, length
+var_export() -outputs string representation of a var
+
+
+VARIABLES*/
+$name = 'Jessie';// string
+$age = 33; //int
+$has_kids = false; //bool
+$cash = 50.50; //float
+
+echo $name . ' is ' . $age . ' years old.';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
